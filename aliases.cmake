9228dd6f387b8f64ff0c2b0e65b21eaeb1bb6138
Mathias Brossard <mathias@brossard.org>
2021-12-24 14:49:51 -0600

    Add support for LPC55S69-EVK

--- aliases.cmake
+++ aliases.cmake
@@ -1,4 +1,5 @@
-#
+# SPDX-License-Identifier: Apache-2.0
 
+set(LPCXpresso55S69_BOARD_ALIAS lpcxpresso55s69_cpu0)
 set(Microbit_BOARD_ALIAS bbc_microbit)
 set(Microbitv2_BOARD_ALIAS bbc_microbit_v2)
--- build.mk
+++ build.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 TARGETS = \
+	LPCXpresso55S69 \
 	Microbit \
 	Microbitv2 \
 
